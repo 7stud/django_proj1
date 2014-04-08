caaa3b1e09c1b8c53682fafd30b44c6f18b67e81
7stud <7stud@excite.com>
2014-04-07 20:35:36 -0600

    Add Poll and Choice models

---
+++ fabfile.py
@@ -0,0 +1,17 @@
+from fabric.api import lcd, local
+
+def prepare_deployment(branch_name):
+    local('python manage.py test proj1')
+    local('git add -p && git commit') # or local('hg add && hg commit')
+
+def deploy():
+    with lcd('/Users/7stud/django_projects/proj1/'):
+
+        # With git...
+        local('git pull /Users/7stud/dev_django_projects/proj1/')
+
+        # With both
+        local('python manage.py migrate myapp')
+        local('python manage.py test myapp')
+        local('python manage.py runserver')
+
